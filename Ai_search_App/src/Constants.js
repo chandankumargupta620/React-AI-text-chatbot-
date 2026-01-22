c4636fe7c089a9392a70880e16ec9d965d673a09
Chandan Kumar Gupta <ck.17112003@gmail.com>
2026-01-22 09:48:40 +0530

    add new api key

--- Ai_search_App/src/Constants.js
+++ Ai_search_App/src/Constants.js
@@ -1,1 +1,1 @@
-export let URL= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBYgyKy9GuIGATTPNAiOOeIFx2t4AgVq-o" ;
+export let URL= "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBNZtG0XiaJ1aYtQ1AqmXSelj-sxddQmiM" ;
